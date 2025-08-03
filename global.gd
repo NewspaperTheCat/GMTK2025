5340bcab99593592dbe43acb4a238e89e52d951a
NewspaperTheCat <jmprendergast@wpi.edu>
2025-08-03 02:21:54 -0500

    Final checks

--- global.gd
+++ global.gd
@@ -6,7 +6,7 @@ var camera_rig : CameraRig
 var audio_controller : AudioController
 var advanced_stats : AdvancedStats
 
-var level_progress : int = 11:
+var level_progress : int = 0:
 	set(value):
 		if value > level_progress:
 			level_progress = value
